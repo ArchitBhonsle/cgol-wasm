8f64caef126248414da7a8e6c7e884a06045e855
Archit Bhonsle <abhonsle2000@gmail.com>
2021-03-03 11:22:15 +0530

    ⌨️  shortcuts, 🛎️ help modal

--- misc.js
+++ misc.js
@@ -1,5 +1,67 @@
+// canvas resize
 const canvas = document.getElementById('canvas');
 canvas.style.width = '100%';
 canvas.style.height = '100%';
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
+
+const pausePlay = document.getElementById('pause-play');
+const randomize = document.getElementById('randomize');
+const clear = document.getElementById('clear');
+const fps_input = document.getElementById('fps-input');
+const help = document.getElementById('help');
+
+// keyboard controls
+window.addEventListener(
+  'keydown',
+  function (event) {
+    if (event.defaultPrevented) return;
+
+    switch (event.key) {
+      case ' ':
+        pausePlay.click();
+        break;
+      case 'Enter':
+        randomize.click();
+        break;
+      case 'Backspace':
+        clear.click();
+        break;
+      case 'ArrowUp':
+        fps_input.stepUp();
+        break;
+      case 'ArrowDown':
+        fps_input.stepDown();
+        break;
+      case '?':
+        help.click();
+        break;
+      default:
+        return;
+    }
+
+    // Cancel the default action to avoid it being handled twice
+    event.preventDefault();
+  },
+  true
+);
+
+// modal
+const modalContainer = document.getElementById('modal-container');
+const modal = document.getElementById('modal');
+const modalClose = document.getElementById('modal-close');
+
+help.onclick = function () {
+  modalContainer.style.display =
+    modalContainer.style.display === 'block' ? 'none' : 'block';
+};
+
+modalClose.onclick = function () {
+  modalContainer.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target == modalContainer) {
+    modalContainer.style.display = 'none';
+  }
+};
